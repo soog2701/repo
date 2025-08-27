b333ce9e866454c02905d50cc6bc1fd4e0440707
soog2701 <cm2spi@naver.com>
2025-08-27 21:18:40 +0900

    next config ssr/api 옵션수정정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
   turbopack: {
     // Turbopack 관련 옵션을 여기에 추가할 수 있습니다.
     // 예: hotReload: true
